481cef1bb8256a2f959f71c3c941489968a1c80c
yjtan0819 <tan.yijia0819@gmail.com>
2024-06-28 12:34:26 -0400

    add vpc

--- EC2.tf
+++ EC2.tf
@@ -7,11 +7,13 @@ resource "aws_instance" "devops-server" {
     instance_type = "t2.micro"
     key_name = "devops-fullstack"
     security_groups = ["${aws_security_group.devops-sg.name}"]
+    subnet_id = "${aws_subnet.devops-public-subnet-01.id}"
 }
 
 resource "aws_security_group" "devops-sg" {
-  name        = "devops-sg"
-  description = "Allow SSH access"
+    name        = "devops-sg"
+    description = "Allow SSH access"
+    vpc_id      = "${aws_vpc.devops-vpc.id}"
 
     ingress {
         from_port   = 22
@@ -33,3 +35,56 @@ resource "aws_security_group" "devops-sg" {
   }
 }
 
+resource "aws_vpc" "devops-vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+        Name = "devops-vpc"
+    }
+  
+}
+
+resource "aws_subnet" "devops-public-subnet-01" {
+    vpc_id = "${aws_vpc.devops-vpc.id}"
+    cidr_block = "10.1.0.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "devops-public-subnet-01"
+    }
+}
+
+resource "aws_subnet" "devops-public-subnet-02" {
+    vpc_id = "${aws_vpc.devops-vpc.id}"
+    cidr_block = "10.1.0.0/24"
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "devops-public-subnet-02"
+    }
+}
+
+resource "aws_internet_gateway" "devops-igw" {
+    vpc_id = "${aws_vpc.devops-vpc.id}"
+    tags = {
+        Name = "devops-igw"
+    }
+}
+
+resource "aws_route_table" "devops-public-rt" {
+    vpc_id = "${aws_vpc.devops-vpc.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.devops-igw.id}"
+    }
+  
+}
+
+resource "aws_route_table_association" "devops-rta-public-subnet-01" {
+    subnet_id = "${aws_subnet.devops-public-subnet-01.id}"
+    route_table_id = "${aws_route_table.devops-public-rt.id}"
+}
+
+resource "aws_route_table_association" "devops-rta-public-subnet-02" {
+    subnet_id = "${aws_subnet.devops-public-subnet-02.id}"
+    route_table_id = "${aws_route_table.devops-public-rt.id}"
+}
