99fddcaa3dc2c825070a06d067fe8e391cc89780
yjtan0819 <tan.yijia0819@gmail.com>
2024-06-28 00:35:56 -0400

    add security group

--- EC2.tf
+++ EC2.tf
@@ -2,8 +2,34 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "demo-server" {
+resource "aws_instance" "devops-server" {
     ami = "ami-08a0d1e16fc3f61ea"
     instance_type = "t2.micro"
     key_name = "devops-fullstack"
+    security_groups = ["${aws_security_group.devops-sg.name}"]
 }
+
+resource "aws_security_group" "devops-sg" {
+  name        = "devops-sg"
+  description = "Allow SSH access"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+  tags = {
+    Name = "ssh_prot"
+  }
+}
+
